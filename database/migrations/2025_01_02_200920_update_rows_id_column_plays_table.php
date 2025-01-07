aac1036f2dd25a2a30af7a544b0876a44cbc1dd2
Jared Peter <aljapeter05@gmail.com>
2025-01-07 15:31:37 -0300

    removed rooms_id that caused error

--- database/migrations/2025_01_02_200920_update_rows_id_column_plays_table.php
+++ database/migrations/2025_01_02_200920_update_rows_id_column_plays_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('plays', function (Blueprint $table) {
-            $table->unsignedBigInteger('room_id')->nullable()->change();
+            // $table->unsignedBigInteger('room_id')->nullable()->change();
         });
     }
 
